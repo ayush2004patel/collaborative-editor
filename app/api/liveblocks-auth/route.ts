19aaa454d1cc718bcd1141c6fc79414be4d57960
Ayush Patel <ayushpatel4021@gmail.com>
2024-08-07 16:08:51 +0530

    fixed a bug

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -22,17 +22,6 @@ export async function POST(request: Request) {
     }
   }
 
-  // Start an auth session inside your endpoint
-//   const session = liveblocks.prepareSession(
-//     user.id,
-//     { userInfo: user.metadata } // Optional
-//   );
-
-  // Use a naming pattern to allow access to rooms with wildcards
-  // Giving the user read access on their org, and write access on their group
-//   session.allow(`${user.organization}:*`, session.READ_ACCESS);
-//   session.allow(`${user.organization}:${user.group}:*`, session.FULL_ACCESS);
-
   // Authorize the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
